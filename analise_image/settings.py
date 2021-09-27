6e440becebe64909c2c52360e8c684c769f30c28
Fabio Oliveira <fabio20rj@gmail.com>
2021-09-27 11:37:39 -0300

    feat: added models Image and analise

--- analise_image/settings.py
+++ analise_image/settings.py
@@ -125,3 +125,14 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [],
+    'TEST_REQUEST_DEFAULT_FORMAT': 'json'
+}
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- core/models.py
+++ core/models.py
@@ -1,3 +1,27 @@
+from random import randint
 from django.db import models
 
-# Create your models here.
+
+def upload_image(filename):
+    return f"{randint(1, 1000000)}-{filename}"
+
+
+STATUS = [("A", "Analisado"), ("N", "Não analisado")]
+
+class Image(models.Model):
+    name = models.CharField("nome", max_length=150)
+    image =  models.ImageField(upload_to=upload_image, blank=True, null=True)
+
+    def __repr__(self) -> str:
+        return self.name
+
+class Analise(models.Model):
+    name = models.CharField("nome", max_length=255)
+    image = models.ForeignKey(Image, on_delete=models.CASCADE)
+    type = models.CharField("tipo", max_length=150)
+    created_at = models.DateField("Criado", auto_created=True)
+    updated_at = models.DateField("Atualizado", auto_now=True)
+    status = models.CharField("status", max_length=15, choices=STATUS)
+
+    def __repr__(self) -> str:
+        return self.name
