96c08dd7fe3dbfa4bb01f1d8b372d61a66a57e1c
Fabio Oliveira <fabio20rj@gmail.com>
2021-09-30 20:30:50 -0300

    update: Changed views to model view set

--- core/views.py
+++ core/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from core.models import Imagem, Analise
+from core.serializers import AnaliseSerializer, ImagemSerializer
+from rest_framework import viewsets
 
-# Create your views here.
+
+class ImagemView(viewsets.ModelViewSet):
+    queryset = Imagem.objects.all()
+    serializer_class = ImagemSerializer
+
+
+class AnaliseView(viewsets.ModelViewSet):
+    queryset = Analise.objects.all()
+    serializer_class = AnaliseSerializer
