fa37db4b72381d29d59a8c6609c136623db43bbb
Fabio Oliveira <fabio20rj@gmail.com>
2021-09-30 20:25:21 -0300

    test: Added test to views and models

--- core/tests/test_models.py
+++ core/tests/test_models.py
@@ -1,50 +1,56 @@
-from django.test import TestCase
+import pytest
 from core.models import Image, Analise
-from django.core.files import File
 from datetime import datetime
 
 
-class ImageTest(TestCase):
-    """Test module for image"""
-
-    def setUp(self) -> None:
-        Image.objects.create(
-            name="teste", 
-            image=File(file=b"")
-        )
-    
-    def test_get_file_name(self):
-        result = Image.objects.get(name="teste")
-        expect = "teste"
-        self.assertEqual(result.name, expect)
-
-        
-    def test_has_one_image(self):
-        result = Image.objects.count()
-        self.assertEqual(result, 1)
+@pytest.mark.django_db
+def test_get_file_name():
+    Image.objects.create(
+        name="teste",
+        image=""
+    )
+    assert Image.objects.get(name="teste").name == "teste"
 
 
-class AnaliseTest(TestCase):
-    """Test module for analise"""
+@pytest.mark.django_db
+def test_has_one_image():
+    Image.objects.create(
+        name="teste",
+        image=""
+    )
+    assert Image.objects.count() == 1
 
-    def setUp(self) -> None:
-        image = Image.objects.create(
-            name="teste", 
-            image=File(file=b"")
-        )
-        Analise.objects.create(
-            name="teste",
-            image=image,
-            type="teste",
-            created_at=datetime.now(),
-            status="A"
-        )
-    def test_get_analise_name(self):
-        result = Analise.objects.get(name="teste")
-        expect = "teste"
-        self.assertEqual(result.name, expect)
 
-    
-    def test_has_one_analise(self):
-        result = Analise.objects.count()
-        self.assertEqual(result, 1)
+@pytest.mark.django_db
+def test_get_analise_name():
+    image = Image.objects.create(
+        name="teste",
+        image=""
+    )
+
+    Analise.objects.create(
+        name="teste",
+        image=image,
+        type="teste",
+        created_at=datetime.now(),
+        updated_at=datetime.now(),
+        status="A"
+    )
+    assert Analise.objects.get(name="teste").name == "teste"
+
+
+@pytest.mark.django_db
+def test_has_one_analise():
+    image = Image.objects.create(
+        name="teste",
+        image=""
+    )
+    Analise.objects.create(
+        name="teste",
+        image=image,
+        type="teste",
+        created_at=datetime.now(),
+        updated_at=datetime.now(),
+        status="A"
+    )
+    assert Image.objects.count() == 1
---
+++ core/tests/test_view.py
@@ -0,0 +1,197 @@
+import json
+from datetime import datetime
+
+import pytest
+from core.models import Analise, Imagem
+from core.serializers import AnaliseSerializer
+from rest_framework import status
+from core.ultis import create_image
+
+
+# initialize the APIClient app
+@pytest.fixture
+def create_models():
+    imagem_memory = create_image()
+
+    imagem = Imagem.objects.create(
+        nome="teste1",
+        imagem=imagem_memory
+    )
+    image2 = Imagem.objects.create(
+        nome="teste2",
+        imagem=imagem_memory
+    )
+    Analise.objects.create(
+        nome="teste",
+        imagem=imagem,
+        tipo="teste",
+        status="A"
+    )
+    Analise.objects.create(
+        nome="teste2",
+        imagem=image2,
+        tipo="teste2",
+        status="A"
+    )
+
+
+@pytest.mark.django_db
+def test_view_get_all_images(client, create_models):
+    url = "http://127.0.0.1:8000/api/v1/imagens/"
+    response = client.get(url)
+    images = Imagem.objects.count()
+    assert response
+    assert images == len(response.data)
+    assert response.status_code == status.HTTP_200_OK
+
+
+@pytest.mark.django_db
+def test_view_get_single_image(client, create_models):
+    url = "http://127.0.0.1:8000/api/v1/imagens/1/"
+    response = client.get(url)
+    assert response
+    assert response.status_code == status.HTTP_200_OK
+
+
+@pytest.mark.django_db
+def test_view_get_wrong_image(client):
+    url = "http://127.0.0.1:8000/api/v1/imagens/30/"
+    response = client.get(url)
+    assert response.status_code == status.HTTP_404_NOT_FOUND
+
+
+@pytest.mark.django_db
+def test_create_new_image(client):
+    image_memory = create_image()
+
+    valid_payload = {
+        "nome": "teste3",
+        "imagem": image_memory
+    }
+    url = "http://127.0.0.1:8000/api/v1/imagens/"
+    response = client.post(
+      url, data=valid_payload
+    )
+    imagem = Imagem.objects.last()
+    assert imagem.nome == "teste3"
+    assert response.status_code == status.HTTP_201_CREATED
+
+
+@pytest.mark.django_db
+def test_try_create_invalid_image(client):
+    invalid_payload = {
+            "nome": 10.0,
+            "imagem": 10.0,
+        }
+    url = "http://127.0.0.1:8000/api/v1/imagens/"
+    response = client.post(url, data=invalid_payload)
+    assert response.status_code == status.HTTP_400_BAD_REQUEST
+
+
+@pytest.mark.django_db
+def test_try_update_wrong_image(client, create_models):
+    invalid_payload = {
+            "teste": "teste",
+            "teste": "teste"
+        }
+    url = "http://127.0.0.1:8000/api/v1/imagens/30/"
+    response = client.put(url, data=invalid_payload)
+    assert response.status_code == status.HTTP_404_NOT_FOUND
+
+
+@pytest.mark.django_db
+def test_delete_image(client, create_models):
+    url = "http://127.0.0.1:8000/api/v1/imagens/1/"
+    response = client.delete(url)
+    assert response.status_code == status.HTTP_204_NO_CONTENT
+
+
+@pytest.mark.django_db
+def test_delete_wrong_image(client, create_models):
+    url = "http://127.0.0.1:8000/api/v1/imagens/30/"
+    response = client.delete(url)
+    assert response.status_code == status.HTTP_404_NOT_FOUND
+
+
+# tests for analises
+@pytest.mark.django_db
+def test_view_get_all_analises(client, create_models):
+    url = "http://127.0.0.1:8000/api/v1/analises/"
+    response = client.get(url)
+    analises = Analise.objects.all()
+    serializer = AnaliseSerializer(analises, many=True)
+
+    assert response.data == serializer.data
+    assert response.status_code == status.HTTP_200_OK
+
+
+@pytest.mark.django_db
+def test_view_get_single_analise(client, create_models):
+    analise = Analise.objects.get(pk=1)
+    url = "http://127.0.0.1:8000/api/v1/analises/1/"
+    response = client.get(url)
+    serializer = AnaliseSerializer(analise)
+    assert response.data == serializer.data
+    assert response.status_code == status.HTTP_200_OK
+
+
+@pytest.mark.django_db
+def test_view_get_wrong_analise(client):
+    url = "http://127.0.0.1:8000/api/v1/analises/10/"
+    response = client.get(url)
+    assert response.status_code == status.HTTP_404_NOT_FOUND
+
+
+@pytest.mark.django_db
+def test_create_new_analise(client, create_models):
+
+    imagem = Imagem.objects.get(pk=2)
+    date = datetime.now()
+    date = date.strftime("%Y-%m-%d")
+    valid_payload = {
+            "nome": "teste1",
+            "imagem": imagem.pk,
+            "tipo": "teste",
+            "status": "A"
+
+        }
+
+    url = "http://127.0.0.1:8000/api/v1/analises/"
+
+    response = client.post(
+        path=url,
+        data=json.dumps(valid_payload),
+        content_type='application/json'
+    )
+    assert response.status_code == status.HTTP_201_CREATED
+
+
+@pytest.mark.django_db
+def test_try_create_invalid_analise(client, create_models):
+    invalid_payload = {
+            "nome": "",
+            "imagem": "",
+            "tipo": "",
+            "status": ""
+        }
+    url = "http://127.0.0.1:8000/api/v1/analises/"
+    response = client.post(
+        url,
+        data=json.dumps(invalid_payload),
+        content_type='application/json'
+    )
+    assert response.status_code == status.HTTP_400_BAD_REQUEST
+
+
+@pytest.mark.django_db
+def test_delete_analise(client, create_models):
+    url = "http://127.0.0.1:8000/api/v1/analises/1/"
+    response = client.delete(url)
+    assert response.status_code == status.HTTP_204_NO_CONTENT
+
+
+@pytest.mark.django_db
+def test_delete_wrong_analise(client, create_models):
+    url = "http://127.0.0.1:8000/api/v1/analises/10/"
+    response = client.delete(url)
+    assert response.status_code == status.HTTP_404_NOT_FOUND
