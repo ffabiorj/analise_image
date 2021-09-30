b96e3f747254a9dda627470f6017a97d100e331e
Fabio Oliveira <fabio20rj@gmail.com>
2021-09-30 20:28:36 -0300

    update: Changed ulrs routers

--- core/urls.py
+++ core/urls.py
@@ -1,10 +1,13 @@
 
 from django.urls import path
+from django.urls.conf import include
 from core import views
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register('imagens', views.ImagemView)
+router.register('analises', views.AnaliseView)
 
 urlpatterns = [
-    path("api/v1/images", views.get_delete_update_image, name="get_delete_update_image"),
-    path("api/v1/images", views.get_post_image, name="get_post_images"),
-    path("api/v1/analises", views.get_delete_update_analise, name="get_delete_update_analise"),
-    path("api/v1/analises", views.get_post_analise, name="get_post_analise")
+    path("api/v1/", include(router.urls))
 ]
