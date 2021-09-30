03404dbb474b6c8b70a7000c9ba50510c42550cf
Fabio Oliveira <fabio20rj@gmail.com>
2021-09-30 20:29:16 -0300

    update: Changed models

--- core/models.py
+++ core/models.py
@@ -2,26 +2,37 @@ from random import randint
 from django.db import models
 
 
-def upload_image(filename):
+STATUS = [("A", "Analisado"), ("N", "Não analisado")]
+
+
+def upload_image(instance, filename):
     return f"{randint(1, 1000000)}-{filename}"
 
 
-STATUS = [("A", "Analisado"), ("N", "Não analisado")]
+class Imagem(models.Model):
+    nome = models.CharField("nome", max_length=150)
+    imagem = models.ImageField(upload_to=upload_image)
 
-class Image(models.Model):
-    name = models.CharField("nome", max_length=150)
-    image =  models.ImageField(upload_to=upload_image, blank=True, null=True)
+    def __repr__(self):
+        return self.nome
 
-    def __repr__(self) -> str:
-        return self.name
+    def __str__(self):
+        return self.nome
+
 
 class Analise(models.Model):
-    name = models.CharField("nome", max_length=255)
-    image = models.ForeignKey(Image, on_delete=models.CASCADE)
-    type = models.CharField("tipo", max_length=150)
-    created_at = models.DateField("Criado", auto_created=True)
-    updated_at = models.DateField("Atualizado", auto_now=True)
-    status = models.CharField("status", max_length=15, choices=STATUS)
+    nome = models.CharField(max_length=255)
+    imagem = models.ForeignKey(
+        Imagem, on_delete=models.CASCADE,
+        related_name="analises"
+    )
+    tipo = models.CharField(max_length=150)
+    criado_em = models.DateField(auto_now_add=True)
+    atualizado_em = models.DateField(auto_now=True)
+    status = models.CharField(max_length=15, choices=STATUS)
 
-    def __repr__(self) -> str:
-        return self.name
+    def __repr__(self):
+        return self.nome
+
+    def __str__(self):
+        return self.nome
