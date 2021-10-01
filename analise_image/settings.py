27c406fedfeb94b86e26ee408b51337aeddf1308
Fabio Oliveira <fabio20rj@gmail.com>
2021-10-01 07:42:21 -0300

    fix: Fixed some issues about libs

--- analise_image/settings.py
+++ analise_image/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import os
 from pathlib import Path
 from decouple import config, Csv
-from dj_database_url import parse as dburl
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
