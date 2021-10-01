d159326333e09e3c4573ee4ff3428cfce36590b2
Fabio Oliveira <fabio20rj@gmail.com>
2021-10-01 00:01:42 -0300

    feat: Added search by name and added tests.

--- core/views.py
+++ core/views.py
@@ -1,6 +1,8 @@
 from core.models import Imagem, Analise
 from core.serializers import AnaliseSerializer, ImagemSerializer
 from rest_framework import viewsets
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.generics import ListAPIView
 
 
 class ImagemView(viewsets.ModelViewSet):
@@ -8,6 +10,20 @@ class ImagemView(viewsets.ModelViewSet):
     serializer_class = ImagemSerializer
 
 
+class ImagemListView(ListAPIView):
+    queryset = Imagem.objects.all()
+    serializer_class = ImagemSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['nome']
+
+
 class AnaliseView(viewsets.ModelViewSet):
     queryset = Analise.objects.all()
     serializer_class = AnaliseSerializer
+
+
+class AnaliseListView(ListAPIView):
+    queryset = Analise.objects.all()
+    serializer_class = AnaliseSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ["nome"]
